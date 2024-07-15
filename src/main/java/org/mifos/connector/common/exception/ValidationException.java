6d6b10489325db530cda11cd3919bb7f884471ea
kaibalya-fynarfin <kaibalya@fynarfin.io>
2024-07-15 18:38:12 +0530

    added lombok for getter in validation exception

--- src/main/java/org/mifos/connector/common/exception/ValidationException.java
+++ src/main/java/org/mifos/connector/common/exception/ValidationException.java
@@ -1,16 +1,13 @@
 package org.mifos.connector.common.exception;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.mifos.connector.common.channel.dto.PhErrorDTO;
 
+@Getter
+@RequiredArgsConstructor
 public class ValidationException extends RuntimeException {
+
     private final PhErrorDTO phErrorDTO;
-
-    public ValidationException(PhErrorDTO phErrorDTO) {
-        this.phErrorDTO = phErrorDTO;
-    }
-
-    public PhErrorDTO getPhErrorDTO() {
-        return phErrorDTO;
-    }
 
 }
